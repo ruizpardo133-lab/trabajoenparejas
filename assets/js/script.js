5b090602f20bad754de7acab323f891ae0132455
pardo133 <pardo133@hotmail.com>
2026-03-23 11:41:46 +0100

    cambio RUTAS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-console.log("hola")
+let rutasEspeciales = ["Bilbao", "Zaragoza", "Cádiz", "Barcelona", "Málaga"];
+rutasEspeciales.slice(2 ,1)
+rutasEspeciales.reverse()
+const resultado = "LOGISTICA".split("").reverse().join("");
+console.log(rutasEspeciales)
+
+
